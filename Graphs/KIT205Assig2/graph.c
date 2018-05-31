72c6ee03c4c6e78c5261106cb64b320811f449e7
Sumit <sumittoshniwal92@gmail.com>
2018-05-31 08:17:44 +0530

    Adding some initial code

--- Graphs/KIT205Assig2/graph.c
+++ Graphs/KIT205Assig2/graph.c
@@ -1,1 +1,15 @@
 ﻿#include "graph.h"
+Graph *g;
+
+void initGraph(int size) {
+    g = (Graph *)malloc(sizeof(Graph) * size * size);
+    for(int i = 0; i < size; i++) {
+        g[i].V = i;
+        g[i].edges = malloc(sizeof(EdgeList));
+        g[i].edges-> head = NULL;
+    }
+}
+
+void addEdge(int from, int to, int weight) {
+   g[i] 
+}
